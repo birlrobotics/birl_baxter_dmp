67af6fa7d676ff276f58b606a5b79eb196c602c8
tonyduan <470963660@qq.com>
2017-12-21 15:40:31 +0800

    fix generalization bug

--- scripts/demo.py
+++ scripts/demo.py
@@ -3,6 +3,10 @@ import pandas as pd
 import numpy as np
 from birl_baxter_dmp.dmp_train import train
 from birl_baxter_dmp.dmp_generalize import dmp_imitate
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import pydmps
+import pydmps.dmp_discrete
 
 import os 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -18,16 +22,36 @@ Colum2_Traj=[float(l.split()[2]) for l in open(data)]
 traj=[[0.0, 0.0, 0.0]]*PointCount
 for i in range(PointCount):
     traj[i]=[Colum0_Traj[i], Colum1_Traj[i], Colum2_Traj[i]]
+y_des = np.array([Colum0_Traj,Colum1_Traj,Colum2_Traj])
+######
+#dmp = pydmps.dmp_discrete.DMPs_discrete(n_dmps=3, n_bfs=500, ay=np.ones(3)*10.0)
+#dmp.imitate_path(y_des=y_des, plot=True)
+#y_track, dy_track, ddy_track = dmp.rollout()
+######
 
-train_set = np.array([traj,traj])
+
+train_set = y_des 
 
 
 param, base_function = train(train_set)
 
 
-start_point = [1.0000000e-02 , -8.0985915e-01 , -7.9287305e-01]
-ending_point = [2.0000000e+00  , 5.0469484e-01  ,-8.0178174e-02]
+start_point = traj[0]
+ending_point = traj[-1]
 
-y = dmp_imitate(starting_pose=start_point, ending_pose=ending_point, weight_mat=param,n_dmps=3 )
-print y
+y_track = dmp_imitate(starting_pose=start_point, ending_pose=ending_point, weight_mat=param,n_dmps=3 )
 
+#creat fig
+fig=plt.figure()
+ax = Axes3D(fig)    
+plt.xlabel('X')
+plt.ylabel('Y')
+
+#plot traj fig 
+ax.plot(Colum0_Traj,Colum1_Traj,Colum2_Traj,linewidth=4,alpha=0.3)       
+#Plot plan fig    
+ax.plot(y_track[:,0],y_track[:,1],y_track[:,2],"--")
+#Plot plan fig    
+#show the plot
+plt.draw()
+plt.show()
--- src/birl_baxter_dmp/dmp_train.py
+++ src/birl_baxter_dmp/dmp_train.py
@@ -1,10 +1,8 @@
-
-
-
 import pydmps.dmp_discrete
 import ipdb
 import pandas as pd
 import numpy as np
+import sys
 # please install pydmp first 
 # https://github.com/studywolf/pydmps
 # input
@@ -15,19 +13,11 @@ import numpy as np
 # w_avarage   list       aveage the weights
 # type_of_basefunc  string      the type of basis fuction
 def train(train_set, n_bfs=100): 
-    w_list = []
-    n_dmps = train_set[0].shape[1]
-    dmp = pydmps.dmp_discrete.DMPs_discrete(dmps=n_dmps, bfs=n_bfs)
-    for trial in train_set:     # for every trial
-        dmp.imitate_path(y_des=trial) #train 7 demonsion
-        w_list.append(dmp.w)
-    w_sum = np.array(w_list)
-    sum_j = 0  #iniliazition
-    for j in w_sum:
-        sum_j += j
-    w_avarage = sum_j/len(w_list)         
+    n_dmps = train_set.shape[0]
+    dmp = pydmps.dmp_discrete.DMPs_discrete(n_dmps=n_dmps, n_bfs=n_bfs)
+    dmp.imitate_path(y_des=train_set) #train 7 demonsion       
     type_of_basefunc = "Gaussian" 
-    return w_avarage, type_of_basefunc     
+    return dmp.w, type_of_basefunc     
 
 
 if __name__ == '__main__':
