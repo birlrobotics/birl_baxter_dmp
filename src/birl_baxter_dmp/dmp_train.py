3b0e69cba8ff6cdcb4ce8196a5f21c402d792cc8
tonyduan <470963660@qq.com>
2017-12-21 16:22:11 +0800

    fix many matrix input bugs

--- src/birl_baxter_dmp/dmp_train.py
+++ src/birl_baxter_dmp/dmp_train.py
@@ -15,17 +15,20 @@ import sys
 def train(train_set, n_bfs=100): 
     n_dmps = train_set.shape[1]
     dmp = pydmps.dmp_discrete.DMPs_discrete(n_dmps=n_dmps, n_bfs=n_bfs)
-    dmp_w = []
+    w_list = []
     for trials in train_set: 
         dmp.imitate_path(y_des=trials) #train 7 demonsion    
-        dmp_w.append(dmp.w)
-    print dmp_w
-    t = []
-    for i in dmp_w:
-        t = t+i
-    t= t/len(dmp_w)
+        w_list.append(dmp.w)
+    print w_list
+    w_sum = np.array(w_list)
+    sum_j = 0  #iniliazition
+    for j in w_sum:
+        sum_j += j
+    w_avarage = sum_j/len(w_list) 
+    ipdb.set_trace()
+    print w_avarage   
     type_of_basefunc = "Gaussian" 
-    return dmp_w, type_of_basefunc     
+    return w_avarage, type_of_basefunc     
 
 
 if __name__ == '__main__':
