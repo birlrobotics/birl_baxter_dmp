16a9df138e80be17147410c71c69502abb9ae954
tonyduan <470963660@qq.com>
2017-12-21 16:12:53 +0800

    test

--- src/birl_baxter_dmp/dmp_train.py
+++ src/birl_baxter_dmp/dmp_train.py
@@ -13,11 +13,19 @@ import sys
 # w_avarage   list       aveage the weights
 # type_of_basefunc  string      the type of basis fuction
 def train(train_set, n_bfs=100): 
-    n_dmps = train_set.shape[0]
+    n_dmps = train_set.shape[1]
     dmp = pydmps.dmp_discrete.DMPs_discrete(n_dmps=n_dmps, n_bfs=n_bfs)
-    dmp.imitate_path(y_des=train_set) #train 7 demonsion       
+    dmp_w = []
+    for trials in train_set: 
+        dmp.imitate_path(y_des=trials) #train 7 demonsion    
+        dmp_w.append(dmp.w)
+    print dmp_w
+    t = []
+    for i in dmp_w:
+        t = t+i
+    t= t/len(dmp_w)
     type_of_basefunc = "Gaussian" 
-    return dmp.w, type_of_basefunc     
+    return dmp_w, type_of_basefunc     
 
 
 if __name__ == '__main__':
